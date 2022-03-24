8864b25516cddb62d040bdc4717cebe13fb1f9d5
Angel-MP <anngylitha_27@hotmail.es>
2022-03-24 09:58:04 -0600

    Declaración de states hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,55 @@
-function App() {
-  let varprueba = "Título"
+import { useState } from "react";
 
-  const handleChangePrueba = (event) => { 
-    //console.log(event.target.name, event.target.value);
-    varprueba = event.target.value
-    //console.log (varprueba);
+function App() {
+  /*let varprueba = "Título";*/
+  //Declaración de state: "hooks"
+  const [tituloState, setTituloState] =  useState("")
+  const [fechaState, setFechaState] =  useState("")
+  const [notaState, setNotaState] =  useState("")
+
+  const handleChangeTitulo= (event) => { 
+    setTituloState(event.target.value);
    }
+
+   const handleChangeFecha = (event) => { 
+    setFechaState(event.target.value);
+   }     
+
+   const handleChangeNota = (event) => { 
+    setNotaState(event.target.value);
+   }  
+
+   
 
   return (
     <div className="App">
       <h3>Notas</h3>
-      <label htmlFor="pruebaID">Input de prueba</label>
-      <input id="pruebaID" name="prueba" type="text" 
-      onChange={handleChangePrueba}
-      value={varprueba}/>
 
+<br />
+<label htmlFor="titulo">Titulo</label>
+      <input 
+        id="titulo" 
+        name="titulo" 
+        type="text" 
+        onChange={handleChangeTitulo}
+        value={tituloState} />
+<br />    
+    <label htmlFor="fecha">Fecha</label>
+      <input 
+        id="fecha" 
+        name="fecha" 
+        type="text" 
+        onChange={handleChangeFecha}
+        value={fechaState} />    
+<br />
+    <label htmlFor="nota">Nota</label>
+      <input 
+        id="nota" 
+        name="nota" 
+        type="text" 
+        onChange={handleChangeNota}
+        value={notaState} />     
+    
     </div>
   );
 }
